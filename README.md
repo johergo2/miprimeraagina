cdab1cbb67d2947e8eff402a6bd9ce15277a484f
johergo2 <johergo2@gmail.com>
2023-11-30 19:24:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# miprimeraagina
+ Mi primera pagina web
---
+++ script.js
@@ -0,0 +1,49 @@
+let menu_responsive = document.querySelector(".checkbtn");
+let selector = "";
+
+
+selector = document.querySelector(".navbar").innerHTML;
+
+
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+
+   console.log(navBar);
+};
+
+/*Validacones Formulario*/
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
+
+
